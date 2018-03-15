ef1c6fae968cd35de06a77eb3c1773ed12a2bbca
guswilliams1 <34108171+guswilliams1@users.noreply.github.com>
2018-03-14 17:16:58 -0700

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,56 @@
+var ClozeCard = require("./ClozeCard.js");
+
+var inquirer = require('inquirer');
+
+var questions = [
+	{
+		type: 'input',
+		name: 'text',
+		message: 'What is the full text for the Cloze Card?',
+		default: function () {
+			return 'Add your full text here.';
+		}
+	},
+	{
+		type: 'input',
+		name: 'cloze',
+		message: 'What is the cloze text for the Cloze Card?',
+		default: function () {
+			return 'Add part of your full text here.';
+		}
+	},
+	{
+			type: 'confirm',
+			name: 'askAgain',
+			message: 'Want to add another Cloze Card?',
+			default: true
+	}
+];
+
+
+function ask() {
+	inquirer.prompt(questions).then(function (answers) {
+		var newCloze = new ClozeCard(answers.text, answers.cloze);
+		var fullText = newCloze.fullText;
+		var cloze = newCloze.cloze;
+		var partialText = newCloze.partial();
+		
+		
+		storeNewCloze(fullText, cloze, partialText);
+		if (answers.askAgain) {
+			ask();
+		} else {
+			console.log("Thanks for adding new flashcard(s)!");
+		}
+	});
+}
+
+ask();
+
+function storeNewCloze(fullText, cloze, partialText) {
+	database.ref().push({
+		fullText: fullText,
+		cloze: cloze,
+		partialText: partialText
+	});
+};
